4ad219fa15a4c51b25a4f7ca002204f5c8ff19cb
Bryan Nielsen <bryan@packettide.com>
2013-11-05 15:38:20 -0500

    script approach is ugly and broken, removing

--- src/Packettide/BreeWysiwyg/BreeWysiwygAdvanced.php
+++ src/Packettide/BreeWysiwyg/BreeWysiwygAdvanced.php
@@ -7,7 +7,8 @@ class BreeWysiwygAdvanced extends FieldType {
 	protected static $assets = array(
 			'packettide/bree-wysiwyg/ckeditor/contents.css',
 			'packettide/bree-wysiwyg/ckeditor/ckeditor.js',
-			'packettide/bree-wysiwyg/ckeditor/adapters/jquery.js'
+			'packettide/bree-wysiwyg/ckeditor/adapters/jquery.js',
+			'packettide/bree-wysiwyg/wysiwyg.advanced.js'
 		);
 
 	public function __construct($name, $data, $options=array())
@@ -20,8 +21,7 @@ class BreeWysiwygAdvanced extends FieldType {
 
 	public function generateField($name, $data, $attributes = array())
 	{
-		return '<textarea name="'.$name.'" type="text" id="'.$name.'" '.$attributes.'>'.$data.'</textarea>'. "\n" .
-				'<script>$(function() { $(".pt-wysiwyg").ckeditor(); });</script>';
+		return '<textarea name="'.$name.'" type="text" id="'.$name.'" '.$attributes.'>'.$data.'</textarea>';
 	}
 
 
